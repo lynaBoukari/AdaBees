bcb376908f972c1b104b6d6b1a51eb74c2f04f03
ilhembekkr <ji_bekkar@esi.dz>
2022-03-26 09:06:29 +0100

    clean the basic code

--- basics.py
+++ basics.py
@@ -6,7 +6,7 @@ import math
 import numpy as np 
  
 
-
+""""
 def findAngle(img, p1, p2, p3, draw=True):
 
         # Get the landmarks
@@ -106,6 +106,48 @@ while(cap.isOpened()):
  
     cv2.imshow("Image", img)
     
+    if cv2.waitKey(1) & 0xFF == ord('q') : 
+        break 
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+"""
+
+
+
+import cv2
+import mediapipe as mp
+import time
+ 
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+ 
+cap = cv2.VideoCapture(0)
+pTime = 0
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    # print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+ 
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+ 
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
+ 
+    cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q') : 
         break 
 
